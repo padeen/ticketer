183cc15b2b858141cca488709aee615049318652
Paulo Adrianus <paulo.deen@gmail.com>
2022-09-27 16:19:41 +0200

    Add admin flag to user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password { "password123" }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
