531218ea7b04df9b0e006f889cc47104d633fc89
Paulo Adrianus <paulo.deen@gmail.com>
2022-10-02 18:53:17 +0200

    Add state model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :ticket
   belongs_to :author, class_name: "User"
+  belongs_to :state, optional: true
 
   scope :persisted, -> { where.not(id:nil) }
   scope :ordered, -> { order(created_at: :asc) }
